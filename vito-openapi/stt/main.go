50dd8355a7f1da8aeaee42eb03d43ad39112eccb
snapbuy <snapbuy@naver.com>
2022-07-09 14:26:59 +0900

    etc

---
+++ vito-openapi/stt/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/vito-ai/go-genproto"
+
+func vito() string{
+	return vito-stt-client.pb()
+}
---
+++ vito/vito/vito.h
@@ -0,0 +1,18 @@
+//
+//  vito.h
+//  vito
+//
+//  Created by 조덕희 on 2022/07/10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for vito.
+FOUNDATION_EXPORT double vitoVersionNumber;
+
+//! Project version string for vito.
+FOUNDATION_EXPORT const unsigned char vitoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <vito/PublicHeader.h>
+
+
